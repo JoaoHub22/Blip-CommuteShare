b63722deb941319721ab1b009b422a8003453a41
JoaoHub22 <al76062@alunos.utad.pt>
2024-04-14 12:30:37 +0100

    Preso em Firebase(páginas múltiplas)

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+    await page.goto('blip---commuteshare.web.app');
 
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
+    await expect(page).toHaveTitle(/CommuteShare/);
 });
 
 test('get started link', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+    await page.goto('blip---commuteshare.web.app');
 
     // Click the get started link.
     await page.getByRole('link', { name: 'Get started' }).click();
