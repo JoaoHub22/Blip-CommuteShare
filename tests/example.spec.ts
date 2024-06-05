7b9beefe96acfafa370a5097621c3160bdec4fc5
JoaoHub22 <al76062@alunos.utad.pt>
2024-06-05 12:08:33 +0100

    playwright working

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,12 +7,12 @@ test('has title', async ({ page }) => {
     await expect(page).toHaveTitle(/CommuteShare/);
 });
 
-test('get started link', async ({ page }) => {
+test('Login into account', async ({ page }) => {
     await page.goto('/');
 
     // Click the get started link.
-    await page.getByRole('link', { name: 'Get started' }).click();
+    await page.getByRole('button', { name: 'Login' }).click();
 
     // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Please sign in' })).toBeVisible();
 });
