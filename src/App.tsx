45386b56e93f25e5fedc451c18fc4d43de0b3d37
JoaoHub22 <al76062@alunos.utad.pt>
2024-06-22 11:22:24 +0100

    Removidas funções não utilizadas em App

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,9 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Routes, Route } from 'react-router-dom';
-import { useContext, useEffect } from 'react';
-import { getToken, onMessage } from 'firebase/messaging';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { collection, getFirestore } from 'firebase/firestore';
 
-import { messaging } from './firebase.ts';
 import imagePath from './assets/CommuteShare.png';
 import NavBar from './components/navbar';
 import Home from './pages/Home.tsx';
@@ -23,13 +19,9 @@ import Perfil from './pages/Profile.tsx';
 import PedirBoleia from './pages/RequestRidePage.tsx';
 import OferecerBoleia from './pages/OfferRidePage.tsx';
 import PerfilEditar from './pages/ProfileEdit.tsx';
-import { AuthContext } from './context/auth-context.tsx';
 
 function App() {
     const items = ['Página Principal', 'Boleias', 'Histórico'];
-    const { currentUser } = useContext(AuthContext);
-    const firestore = getFirestore();
-    const Topics = collection(firestore, 'MessageTopics');
 
     return (
         <>
